f8383a749aec59e61f1253d2f93ecc124c68394d
Danny Walden <walden.houston@gmail.com>
2017-05-16 09:27:00 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'pw0CZtnNs48bDgbyNpSwKbYVE',
-  consumer_secret: 'iYxNgd4MpCIIAUWDwtxYUsfhOqryTEHUOvS7mcyrcVGr2tRriq',
-  access_token_key: '2610088452-XkAxQeOJBA1IvfaKielZtyAUobxKEDjnFLmhENh',
-  access_token_secret: 'n1XgjStQHhQXINm5OcKx7sg0woz2YuSMF0gPSjfTSL84K',
+  consumer_key: ,
+  consumer_secret: ,
+  access_token_key: ,
+  access_token_secret: ,
 }
